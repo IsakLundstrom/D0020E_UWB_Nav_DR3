3b4720d049641cb7d05eae0f4124936e021bbe79
IsakLundstrom <isalun-9@student.ltu.se>
2022-01-20 09:31:24 +0000

    Add config file, alert system; new filename med Hj

--- systemHandler.py
+++ systemHandler.py
@@ -3,14 +3,18 @@ import time
 import json
 import datetime
 import re
+import toml
+from notify_run import Notify 
 
-AVERAGE_TIME_WINDOW_SIZE = 5
-ALERT_HEIGT = 0.5
-ALERT_WINDOW_SIZE = 5
-ALERT_TIME_GAP = datetime.timedelta(minutes = 1/6)
-ALERT_TIME_GAP_START = datetime.timedelta(minutes = 1/12)
+CONFIG = toml.load('config.toml')
 
-class PositionHandler:
+AVERAGE_TIME_WINDOW_SIZE = int(CONFIG['constants']['AVERAGE_TIME_WINDOW_SIZE'])
+ALERT_HEIGT = float(CONFIG['constants']['ALERT_HEIGT'])
+ALERT_WINDOW_SIZE = int(CONFIG['constants']['ALERT_WINDOW_SIZE'])
+ALERT_TIME_GAP = datetime.timedelta(minutes = float(CONFIG['constants']['ALERT_TIME_GAP']))
+ALERT_TIME_GAP_START = datetime.timedelta(minutes = float(CONFIG['constants']['ALERT_TIME_GAP_START']))
+
+class SystemHandler:
     def __init__(self, spotD3, spotUser):
         self.spotD3 = spotD3
         self.spotUser = spotUser
@@ -21,19 +25,24 @@ class PositionHandler:
         self.wasAlerted = True
         self.alertTimeCooldown = datetime.datetime.now() - ALERT_TIME_GAP + ALERT_TIME_GAP_START
 
+        self.notify = Notify() 
+
         print(datetime.datetime.now())
         print(self.alertTimeCooldown)
 
-        broker_address = "130.240.74.55"
+        
+
+
+        broker_address = CONFIG['widefind']['broker_address']
         client = mqtt.Client() 
         client.on_message=self.on_message
         client.connect(broker_address)
         # client.loop_start()
-        client.subscribe("ltu-system/#")
+        client.subscribe(CONFIG['widefind']['subscribe_address'])
         time.sleep(1) # wait
         client.loop_forever()
 
-    def movingAverage(self, currentTime, newZ):
+    def updateMovingZAverage(self, currentTime, newZ):
         # Remove old z cords until the diffrence is less then AVERAGE_TIME_WINDOW_SIZE
         for x in list(self.zCords): 
             delta = currentTime - x
@@ -62,7 +71,6 @@ class PositionHandler:
         mqttMsgString = message.payload.decode()
         mqttMsgJson = json.loads(mqttMsgString)
         # print(mqttMsgJson)
-        #self.data_queue.put(mqttMsgJson)
         jsonMessage = json.dumps(mqttMsgJson)
         if self.isSpotUser(jsonMessage):
             # print(str(message.payload.decode("utf-8")))
@@ -71,23 +79,17 @@ class PositionHandler:
             currentTime = self.getTime(jsonMessage)
             # print(currentTime)
             zCord = cordinates[2]
-            self.movingAverage(currentTime, zCord)
+            self.updateMovingZAverage(currentTime, zCord)
             # Alert if Z height is low and enough measurments and no alert cooldown
             if(self.checkZHeight(zCord) and len(self.zCords) >= ALERT_WINDOW_SIZE and self.checkAlertTimeCooldown(currentTime)):
                 self.updateAlertTimeCooldown(currentTime)
                 self.alert()
             # print('')
 
-        
-        # print("message topic=",message.topic)
-        # print("message qos=",message.qos)
-        # print("message retain flag=",message.retain)
-    ########################################
-
     def checkAlertTimeCooldown(self, currentTime):
         # print(currentTime)
         delta = currentTime - self.alertTimeCooldown
-        print('delta ', delta.seconds)
+        # print('delta ', delta.seconds)
         if(delta > ALERT_TIME_GAP):
             return True
         return False
@@ -102,6 +104,7 @@ class PositionHandler:
 
     def alert(self):
         print('ALERT!')
+        self.notify.send("I've fallen, and I can't get up!")
 
     # Returns cordinates of Widefind mqtt data
     def getCordinates(self, jsonData):
@@ -124,6 +127,8 @@ class PositionHandler:
 
 
 if __name__ == '__main__':
-    p = PositionHandler('vet-inte', 'DA7A27076AF8BBD2')
+    spotD3 = CONFIG['widefind']['spotD3']
+    spotUser = CONFIG['widefind']['spotUser']
+    s = SystemHandler(spotD3, spotUser)
 
 
