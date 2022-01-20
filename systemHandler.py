8749a015182996cd322d1639da1cbf931fc6a1ec
IsakLundstrom <isalun-9@student.ltu.se>
2022-01-20 10:14:10 +0000

    Refactored, renamed and added comments med Hjalmar

--- systemHandler.py
+++ systemHandler.py
@@ -8,6 +8,7 @@ from notify_run import Notify
 
 CONFIG = toml.load('config.toml')
 
+# Determine constants from config file
 AVERAGE_TIME_WINDOW_SIZE = int(CONFIG['constants']['AVERAGE_TIME_WINDOW_SIZE'])
 ALERT_HEIGT = float(CONFIG['constants']['ALERT_HEIGT'])
 ALERT_WINDOW_SIZE = int(CONFIG['constants']['ALERT_WINDOW_SIZE'])
@@ -15,23 +16,21 @@ ALERT_TIME_GAP = datetime.timedelta(minutes = float(CONFIG['constants']['ALERT_T
 ALERT_TIME_GAP_START = datetime.timedelta(minutes = float(CONFIG['constants']['ALERT_TIME_GAP_START']))
 
 class SystemHandler:
-    def __init__(self, spotD3, spotUser):
-        self.spotD3 = spotD3
-        self.spotUser = spotUser
+    def __init__(self):
+        # Load spot ids
+        self.spotD3 = CONFIG['widefind']['spotD3']
+        self.spotUser = CONFIG['widefind']['spotUser']
 
-        self.zCords = {}
-        self.zAvg = 0
+        self.zCordinates = {} # Dictionary: key = time for gotten mqtt msg, value = Z value in mqtt msg
+        self.zAverage = 0
 
-        self.wasAlerted = True
-        self.alertTimeCooldown = datetime.datetime.now() - ALERT_TIME_GAP + ALERT_TIME_GAP_START
+        self.wasAlerted = True # Set to True to not alert until first cooldown
+        self.alertTimeCooldown = datetime.datetime.now() - ALERT_TIME_GAP + ALERT_TIME_GAP_START # Calculates initial cooldown
 
         self.notify = Notify() 
 
-        print(datetime.datetime.now())
-        print(self.alertTimeCooldown)
-
-        
-
+        # print(datetime.datetime.now())
+        # print(self.alertTimeCooldown)
 
         broker_address = CONFIG['widefind']['broker_address']
         client = mqtt.Client() 
@@ -39,57 +38,48 @@ class SystemHandler:
         client.connect(broker_address)
         # client.loop_start()
         client.subscribe(CONFIG['widefind']['subscribe_address'])
-        time.sleep(1) # wait
+        # time.sleep(1) # wait
+        print('System Started')
         client.loop_forever()
 
-    def updateMovingZAverage(self, currentTime, newZ):
-        # Remove old z cords until the diffrence is less then AVERAGE_TIME_WINDOW_SIZE
-        for x in list(self.zCords): 
+    def updateMovingZAverage(self, currentTime, currentZCord):
+        # Remove old z cords until the difference is less then AVERAGE_TIME_WINDOW_SIZE
+        for x in list(self.zCordinates): 
             delta = currentTime - x
             if(delta.seconds > AVERAGE_TIME_WINDOW_SIZE):
-                newsum = self.zAvg*len(self.zCords) - self.zCords[x]
-                del self.zCords[x]
-                if(len(self.zCords) == 0):
-                    self.zAvg = 0
+                newSum = self.zAverage*len(self.zCordinates) - self.zCordinates[x] # We continually recalculate the average Z value for every removed element
+                del self.zCordinates[x]
+                if(len(self.zCordinates) == 0): # Edge case to avoid dividing by zero
+                    self.zAverage = 0
                 else:
-                    self.zAvg = newsum/len(self.zCords)
+                    self.zAverage = newSum/len(self.zCordinates) # Normal execution
             else:
                 break
 
         # Update new average with a new time and cords
-        newsum = self.zAvg*len(self.zCords) + newZ
-        self.zCords.update({currentTime: newZ})
-        self.zAvg = newsum/len(self.zCords)
-        # print('Average Z: ', self.zAvg)
-        # print('')
+        newSum = self.zAverage*len(self.zCordinates) + currentZCord
+        self.zCordinates.update({currentTime: currentZCord})
+        self.zAverage = newSum/len(self.zCordinates)
 
-    def isSpotUser(self, jsonMessage):
-        return self.spotUser in jsonMessage
+    def isSpotUser(self, jsonMsg):
+        return self.spotUser in jsonMsg
     
     def on_message(self, client, userdata, message):
-        # print(str(message.payload.decode("utf-8"))) # Prints all Widefind mqtt data
         mqttMsgString = message.payload.decode()
         mqttMsgJson = json.loads(mqttMsgString)
-        # print(mqttMsgJson)
-        jsonMessage = json.dumps(mqttMsgJson)
-        if self.isSpotUser(jsonMessage):
-            # print(str(message.payload.decode("utf-8")))
-            cordinates = self.getCordinates(jsonMessage)
-            # print(cordinates)
-            currentTime = self.getTime(jsonMessage)
-            # print(currentTime)
-            zCord = cordinates[2]
-            self.updateMovingZAverage(currentTime, zCord)
+        jsonMsg = json.dumps(mqttMsgJson)
+        if self.isSpotUser(jsonMsg):
+            cordinates = self.getCordinates(jsonMsg) # cordinates = [x, y, z]
+            currentTime = self.getTime(jsonMsg) # currenTime = A datetime variable
+            currentZCord = cordinates[2]
+            self.updateMovingZAverage(currentTime, currentZCord)
             # Alert if Z height is low and enough measurments and no alert cooldown
-            if(self.checkZHeight(zCord) and len(self.zCords) >= ALERT_WINDOW_SIZE and self.checkAlertTimeCooldown(currentTime)):
+            if(self.checkZHeight(currentZCord) and len(self.zCordinates) >= ALERT_WINDOW_SIZE and self.isAlertTimeOffCooldown(currentTime)):
                 self.updateAlertTimeCooldown(currentTime)
                 self.alert()
-            # print('')
 
-    def checkAlertTimeCooldown(self, currentTime):
-        # print(currentTime)
+    def isAlertTimeOffCooldown(self, currentTime):
         delta = currentTime - self.alertTimeCooldown
-        # print('delta ', delta.seconds)
         if(delta > ALERT_TIME_GAP):
             return True
         return False
@@ -97,20 +87,18 @@ class SystemHandler:
     def updateAlertTimeCooldown(self, currentTime):
         self.alertTimeCooldown = currentTime
 
-
-    def checkZHeight(self, zCord):
-        return(self.zAvg < ALERT_HEIGT and zCord < ALERT_HEIGT)
-
+    # Average and current Z heigt is below set alert height
+    def checkZHeight(self, currentZCord):
+        return(self.zAverage < ALERT_HEIGT and currentZCord < ALERT_HEIGT)
 
     def alert(self):
-        print('ALERT!')
-        self.notify.send("I've fallen, and I can't get up!")
+        print('ALERT! Fall occured!!')
+        self.notify.send("F")
 
     # Returns cordinates of Widefind mqtt data
-    def getCordinates(self, jsonData):
-        parse = json.loads(jsonData)
+    def getCordinates(self, jsonMqttMsg):
+        parse = json.loads(jsonMqttMsg)
         messageData = parse['message']
-        # print(messageData)
         splitMessageData = messageData.split(",")
         x = (float(splitMessageData[2])/1000)
         y = (float(splitMessageData[3])/1000)
@@ -118,8 +106,8 @@ class SystemHandler:
         return [x, y, z]
 
     # Returns time of Widefind mqtt data
-    def getTime(self, jsonData):
-        parse = json.loads(jsonData)
+    def getTime(self, jsonMqttMsg):
+        parse = json.loads(jsonMqttMsg)
         timeData = parse['time']
         splitTimeData = re.split(r"[-T:.Z]\s*", timeData)
         currentTime = datetime.datetime(int(splitTimeData[0]), int(splitTimeData[1]), int(splitTimeData[2]), int(splitTimeData[3]), int(splitTimeData[4]), int(splitTimeData[5]), int(splitTimeData[6][:6]))
@@ -127,8 +115,6 @@ class SystemHandler:
 
 
 if __name__ == '__main__':
-    spotD3 = CONFIG['widefind']['spotD3']
-    spotUser = CONFIG['widefind']['spotUser']
-    s = SystemHandler(spotD3, spotUser)
+    s = SystemHandler()
 
 
