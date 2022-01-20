40a0ad48856eba08688a8ac20833c0a6ab544295
oskar-dah <oskdah-9@student.ltu.se>
2022-01-20 10:35:23 +0000

    navigation functions, med Pehr

--- example.py
+++ example.py
@@ -1,31 +1,37 @@
 import double
 import sys
-
+from navigate import navigate
 d3 = double.DRDoubleSDK()
+nav = navigate(0,0.5)
 
-try:
+
+nav.navigation(0, 0.5)
+#nav.cancelNavigation()
+""" try:
     d3.sendCommand('events.subscribe', { 'events': [
-        'DRBase.status',
-        'DRCamera.enable'
-    ]})
-    d3.sendCommand('screensaver.nudge');
-    d3.sendCommand('camera.enable', { 'template': 'screen' });
-    d3.sendCommand('base.requestStatus');
+        'DRNavigateModule.targetState',
+        'DRNavigateModule.newTarget'
+    ]})  
+    d3.sendCommand('screensaver.nudge')
+    d3.sendCommand('camera.enable', { 'template': 'screen' })
+    d3.sendCommand('base.requestStatus')
     d3.sendCommand('navigate.enable')
-    d3.sendCommand('navigate.target', {'x':float(0.5),'y':float(0.5),'angleRadians':float(0),'relative':True,'dock':False,'dockId':0})
+    d3.sendCommand('dockTracker.enable')
+    d3.sendCommand('navigate.target', {'x':float(0.5),'y':float(0),'angleRadians':float(0),'relative':True,'dock':False,'dockId':0})
     # d3.sendCommand('navigate.target', {'x':float(-0.5),'y':float(-0.5),'angleRadians':float(6.28),'relative':True,'dock':False,'dockId':0})
     while True:
         packet = d3.recv()
         if packet != None:
             event = packet['class'] + '.' + packet['key']
-            if event == 'DRBase.status':
+            if event == 'DRNavigateModule.targetState':
                 print(packet['data'])
-            elif event == 'DRCamera.enable':
-                print('camera enabled')
+            elif event == 'DRNavigateModule.newTarget':
+                print(packet['data'])
+                print('HejHejHej')
 
 except KeyboardInterrupt:
-    d3.sendCommand('camera.disable');
-    d3.sendCommand('screensaver.nudge');
+    d3.sendCommand('camera.disable')
+    d3.sendCommand('screensaver.nudge')
     d3.close()
     print('cleaned up')
-    sys.exit(0)
+    sys.exit(0)  """
--- navigate.py
+++ navigate.py
@@ -1,25 +1,81 @@
 import double
 import sys
+import math
 
 class navigate():
-    def __init__(self):
+    def __init__(self, yStart, xStart):
         self.d3 = double.DRDoubleSDK()
-#        self.y 
-#        self.x
+        self.setup()
+        self.yStart = yStart
+        self.xStart = xStart
      
+    def setup(self):
+        self.d3.sendCommand('navigate.enable')
+        self.d3.sendCommand('navigate.obstacleAvoidance.setLevel', { "level": 2 })
+        #self.d3.sendCommand('navigate.exitDock')
+        #self.d3.sendCommand('base.kickstand.retract')
+        #self.d3.sendCommand('navigate.target', {'x':float(-1),'y':float(0),'angleRadians':float(math.pi),'relative':True,'dock':False,'dockId':0})
+        self.d3.sendCommand('pose.resetOrigin')
 
     def navigation(self, xCordinate, yCordinate):
         #Positivt värde på X så kör roboten framåt, från origin
         #Negativt värde på X så kör roboten bakåt, från origin
         #Positivt värde på Y så kör roboten vänster, från origin
         #Negativt värde på Y så kör roboten höger, från origin
+
         try:
-            self.d3.sendCommand('navigate.enable')
-            self.d3.sendCommand('navigate.target', {'x':float(xCordinate),'y':float(yCordinate),'angleRadians':float(3.1415),'relative':False,'dock':False,'dockId':0})    
+            self.d3.sendCommand('events.subscribe', { 'events': [
+            'DRNavigateModule.target', 'DRNavigateModule.arrive', 'DRNavigateModule.status', 'DRPose.pose'
+            ]})  
+            
+            self.d3.sendCommand('base.travel.start')
+            self.d3.sendCommand('navigate.target', {'x':float(xCordinate),'y':float(yCordinate),'angleRadians':float(0),'relative':False,'dock':False,'dockId':0})    
+
+            while True:
+                packet = self.d3.recv()
+                if packet != None:
+                    event = packet['class'] + '.' + packet['key']
+                    if event == 'DRPose.pose':
+                        print(packet['data']['base']['x'])
+                        print(packet['data']['base']['y'])
+                        print("--------------->Jag har nått till destinationen<-----------------")
+                    elif event == 'DRNavigateModule.status': 
+                        print('Navigate.Status-------> ', packet['data'])
+                    elif event == 'DRNavigateModule.target':
+                        print('Navigate.target-------> ', packet['data'])
+                    
         except KeyboardInterrupt:
             self.d3.close()
             print('cleaned up')
             sys.exit(0)
+
+    def cancelNavigation(self):
+        #self.d3.sendCommand('navigate.enable')
+        self.d3.sendCommand('navigate.cancelTarget')
+        print("stop!!!")
+
+    def driveHome(self):
+        navigation(self.xStart, self.yStart)
+
+
+"""  def hitTest(self):
+        self.d3.sendCommand('events.subscribe', { 'events': [
+            'DRCamera.hitResult'
+            ]})
+        self.d3.sendCommand('camera.hitTest', {'hit':'true', "x": 0.5, "y": 0.5, "highlight": 'true', "passToNavigate": 'true'})
+        while True:
+               packet = self.d3.recv()
+               if packet != None:
+                   event = packet['class'] + '.' + packet['key']
+                   if event == 'DRCamera.hitResult':
+                       print(packet['data'])
+                       print("nu händer det något")
+                        
+        
+
+
+    def hitNav(self, xHit, yHit):
+        self.d3.sendCommand('navigate.hitResult', {"hit":True, "x":0, "y":0, "z":0}) """
 
 
 
