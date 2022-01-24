cdf8394a82a00b653a474f3988bf5c19bc8886f6
IsakLundstrom <isalun-9@student.ltu.se>
2022-01-24 11:37:33 +0000

    Test to add D3 drive with mqtt data

--- systemHandler.py
+++ systemHandler.py
@@ -1,3 +1,5 @@
+import double
+import sys
 import paho.mqtt.client as mqtt 
 import time
 import json
@@ -5,6 +7,7 @@ import datetime
 import re
 import toml
 from notify_run import Notify 
+from navigate import navigate
 
 CONFIG = toml.load('config.toml')
 
@@ -21,8 +24,11 @@ class SystemHandler:
         self.spotD3 = CONFIG['widefind']['spotD3']
         self.spotUser = CONFIG['widefind']['spotUser']
 
-        self.zCordinates = {} # Dictionary: key = time for gotten mqtt msg, value = Z value in mqtt msg
-        self.zAverage = 0
+        self.zUser = {} # Dictionary: key = time for gotten mqtt msg, value = Z value in mqtt msg
+        self.zUserAverage = 0
+        self.xD3 = None
+        self.yD3 = None
+        self.zD3 = None
 
         self.wasAlerted = True # Set to True to not alert until first cooldown
         self.alertTimeCooldown = datetime.datetime.now() - ALERT_TIME_GAP + ALERT_TIME_GAP_START # Calculates initial cooldown
@@ -44,39 +50,50 @@ class SystemHandler:
 
     def updateMovingZAverage(self, currentTime, currentZCord):
         # Remove old z cords until the difference is less then AVERAGE_TIME_WINDOW_SIZE
-        for x in list(self.zCordinates): 
+        for x in list(self.zUser): 
             delta = currentTime - x
             if(delta.seconds > AVERAGE_TIME_WINDOW_SIZE):
-                newSum = self.zAverage*len(self.zCordinates) - self.zCordinates[x] # We continually recalculate the average Z value for every removed element
-                del self.zCordinates[x]
-                if(len(self.zCordinates) == 0): # Edge case to avoid dividing by zero
-                    self.zAverage = 0
+                newSum = self.zUserAverage*len(self.zUser) - self.zUser[x] # We continually recalculate the average Z value for every removed element
+                del self.zUser[x]
+                if(len(self.zUser) == 0): # Edge case to avoid dividing by zero
+                    self.zUserAverage = 0
                 else:
-                    self.zAverage = newSum/len(self.zCordinates) # Normal execution
+                    self.zUserAverage = newSum/len(self.zUser) # Normal execution
             else:
                 break
 
         # Update new average with a new time and cords
-        newSum = self.zAverage*len(self.zCordinates) + currentZCord
-        self.zCordinates.update({currentTime: currentZCord})
-        self.zAverage = newSum/len(self.zCordinates)
+        newSum = self.zUserAverage*len(self.zUser) + currentZCord
+        self.zUser.update({currentTime: currentZCord})
+        self.zUserAverage = newSum/len(self.zUser)
 
     def isSpotUser(self, jsonMsg):
         return self.spotUser in jsonMsg
+
+    def isSpotD3(self, jsonMsg):
+        return self.spotD3 in jsonMsg
     
     def on_message(self, client, userdata, message):
         mqttMsgString = message.payload.decode()
         mqttMsgJson = json.loads(mqttMsgString)
         jsonMsg = json.dumps(mqttMsgJson)
+        # print(jsonMsg)
         if self.isSpotUser(jsonMsg):
             cordinates = self.getCordinates(jsonMsg) # cordinates = [x, y, z]
+            print('User', cordinates)
             currentTime = self.getTime(jsonMsg) # currenTime = A datetime variable
             currentZCord = cordinates[2]
             self.updateMovingZAverage(currentTime, currentZCord)
-            # Alert if Z height is low and enough measurments and no alert cooldown
-            if(self.checkZHeight(currentZCord) and len(self.zCordinates) >= ALERT_WINDOW_SIZE and self.isAlertTimeOffCooldown(currentTime)):
+            # Alert if Z height is low and enough #measurments and no alert cooldown
+            if(self.checkZHeight(currentZCord) and len(self.zUser) >= ALERT_WINDOW_SIZE and self.isAlertTimeOffCooldown(currentTime)):
                 self.updateAlertTimeCooldown(currentTime)
-                self.alert()
+                self.alert(cordinates[0], cordinates[1])
+        if self.isSpotD3(jsonMsg):
+            cordinates = self.getCordinates(jsonMsg) # cordinates = [x, y, z]
+            self.xD3 = cordinates[0]
+            self.yD3 = cordinates[1]
+            self.zD3 = cordinates[2]
+            print('D3', cordinates)
 
     def isAlertTimeOffCooldown(self, currentTime):
         delta = currentTime - self.alertTimeCooldown
@@ -89,15 +106,36 @@ class SystemHandler:
 
     # Average and current Z heigt is below set alert height
     def checkZHeight(self, currentZCord):
-        return(self.zAverage < ALERT_HEIGT and currentZCord < ALERT_HEIGT)
+        return(self.zUserAverage < ALERT_HEIGT and currentZCord < ALERT_HEIGT)
 
-    def alert(self):
-        print('ALERT! Fall occured!!')
-        self.notify.send("F")
+    def alert(self, targetX, targetY):
+        print('ALERT! Fall!')
+        sendstr = "Fall detected!\nThe target is at (" + str(targetX) + ", " + str(targetY) + ")!"
+        print(sendstr)
+        self.notify.send(sendstr)
+        if(self.xD3 != None and self.yD3 != None):
+            # x = targetX - self.xD3
+            # y = targetY - self.yD3
+            # print("Coordinates to travel (in Widefind)")
+            # print("X:", x, "Y:", y)
+
+            d3 = double.DRDoubleSDK()
+            nav = navigate(0,0)
+            
+            widefindStart = [self.xD3, self.yD3]
+            widefindDest = [targetX, targetY]
+            # widefindStart = [0.677, -2.656]
+            # widefindDest = [1.77, -1.94]
+            print(widefindStart, ' --> ', widefindDest)
+            arr2 = nav.calcWFtoD3(widefindStart, widefindDest)
+
+            nav.navigation(arr2[0], arr2[1])
+
 
     # Returns cordinates of Widefind mqtt data
     def getCordinates(self, jsonMqttMsg):
         parse = json.loads(jsonMqttMsg)
+        # print(parse)
         messageData = parse['message']
         splitMessageData = messageData.split(",")
         x = (float(splitMessageData[2])/1000)
